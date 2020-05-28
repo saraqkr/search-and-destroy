f5f772ed5a19e64f6561eadd66ca732fc986b885
Vicky Rodriguez <vrod8763@gmail.com>
2020-05-28 17:57:05 -0400

    update isLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,25 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  console.log("LINKEDLIST", linkedlist)
+  if(linkedlist.head === null) return false;
+  const firstNode = linkedlist.getNthNode(1)
+  if(firstNode.next === null) return false;
+  let n = 1;
+  if(firstNode.next) {
+    n++
+    const nthNode = linkedlist.getNthNode(n)
+    if (!nthNode.next) {
+      return false;
+    } else {
 
+    }
+  }
 };
-
+//linkedlist.head === linkedlist.head.next
+//if Linkedlist.head === null || node's next == null => return false;
+//if two node's next val === the same node, there is a loop
+//
 
 /*
 EXTRA CREDIT:
