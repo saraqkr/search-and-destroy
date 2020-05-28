b1655d9a584153f2b9ea3014fe0220eaf3eac429
Jianna Park <jiannajihyunpark@gmail.com>
2020-05-28 16:44:28 -0400

    implement binary search function with O(1) space

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,46 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
-};
+const binarySearch = (array, target, firstIdx = 0, lastIdx = array.length) => {
+	// len = 10 --> lastIdx = 9 --> centerIdx = 4
+	// len = 9 --> lastIdx = 8 --> centerIdx = 4
+
+	let center = Math.floor((firstIdx + lastIdx)/2)	// 12(6) -> 18(9) -> 19(10) -> 20(11)
+
+	// if (target < array[0] || target > array[array.length - 1]) return false
+	if (lastIdx < firstIdx) return false
+
+// const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
+// target = 10
+
+	if (target === array[center]) return true
+	else if (array[center] > target) {
+		lastIdx = center - 1		// 4
+		return binarySearch(array, target, firstIdx, lastIdx)
+	} else {
+		firstIdx = center + 1		// firstIdx = 12
+		return binarySearch(array, target, firstIdx, lastIdx)
+	}
+}
+
+
+
+
+
+// const binarySearch = (array, target) => {
+	// get the center index
+	// compare target with that center
+	// if (target === array[center]) return true
+
+	// else
+
+	// if target < center, binarySearch(array, target) (if we were to use recursion, split array into two subarrays && Vicky found another way to use recursion without creating new arrays!)
+
+	// get a new center between far left index & current center, and compare target with the new center element
+// };
+
+
+
 
 /*
 	EXTRA CREDIT:
